91516d4513d42d2dab5e5179070d508e3d242af4
rjynth <hleyn@outlook.com>
2025-04-29 18:13:09 +0300

    simple password check

--- bunkergame/bunkergame/settings.py
+++ bunkergame/bunkergame/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_password_validators',
     'bunkergame',
     'accounts',
     'lobbys',
@@ -110,6 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+{
+        'NAME': 'django_password_validators.password_character_requirements.password_validation.PasswordCharacterValidator',
+        'OPTIONS': {
+             'min_length_digit': 1,
+         }
+    },
+
 ]
 
 
