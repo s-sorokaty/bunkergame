8e0c6e6b138affe3980b24ce5007cf5320e097d4
rjynth <hleyn@outlook.com>
2025-04-29 18:13:09 +0300

    simple password check

--- bunkergame/accounts/forms.py
+++ bunkergame/accounts/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
 
 class UserRegistrationForm(forms.ModelForm):
     username = forms.CharField(label='Имя пользователя:')
-    password = forms.CharField(label='Пароль:', widget=forms.PasswordInput)
-    
+    password = forms.CharField(
+        label='Пароль:',
+        widget=forms.PasswordInput,
+        validators=[validate_password],
+        help_text='Пароль должен содержать хотя бы одну цифру'
+    )
 
     class Meta:
         model = User
