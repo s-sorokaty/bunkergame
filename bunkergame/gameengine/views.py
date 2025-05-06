dd157a4b84354d28fe0fe45d42b2490b22614311
s-sorokaty <sorokaty0824@gmail.com>
2025-05-06 12:59:38 +0700

    feat: added turning game cycle

--- bunkergame/gameengine/views.py
+++ bunkergame/gameengine/views.py
@@ -1,21 +1,15 @@
 from uuid import UUID
-from django.contrib import messages
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, decorators
 from django.http import JsonResponse, HttpRequest, HttpResponseBadRequest
 
 from .models import GameEngine, GameUser
-from .consumers import send_game_message
+from .consumers import sync_game, send_info
 from .utils import exceptions
 
-def sync_game(request, game_id:UUID):
-    game_engine = GameEngine.objects.get(game_id=game_id)
-    game_users = GameUser.objects.filter(game_id=game_engine.game_id).all()
-    user = User.objects.get(id=request.user.id)
 
-    send_game_message(game_id, {"game_users": [game_user.as_ru_dict() for game_user in game_users],
-                                "game_info":{**game_engine.get_game_info(), "request_username":user.username}})
+
 
 @decorators.login_required(login_url='/accounts/login/')
 def create_empty_game(request:HttpRequest):
@@ -27,6 +21,7 @@ def create_empty_game(request:HttpRequest):
         game_engine.create_empty_game(user, game_name, user_count)
         return redirect(f"/game/join/{game_engine.game_id}")
     else: return HttpResponseBadRequest("Wrong request method") 
+
 
 @decorators.login_required(login_url='/accounts/login/')
 def join_game(request:HttpRequest, game_id:UUID):
@@ -40,7 +35,7 @@ def join_game(request:HttpRequest, game_id:UUID):
             
             game_users = GameUser.objects.filter(game_id=game_engine.game_id).all()
         
-            sync_game(request, game_id)
+            sync_game(game_id)
             return render(request, "game.html", {"game_users": [game_user.as_ru_dict() for game_user in game_users],
                                              "game_info":{**game_engine.get_game_info(), "request_username":user.username},
                                              
@@ -61,9 +56,11 @@ def start_game(request:HttpRequest, game_id:UUID):
     try:
         game_engine = GameEngine.objects.get(game_id=game_id)
         game_engine.start_game()
+        send_info(game_engine.game_id, 'Игра началась')
+        sync_game(game_id)
         return JsonResponse({'message':'Игра началась'}, status=200)
     except exceptions.LobbyStatusCheckMismatch as e:
-        return JsonResponse({'err':'Не получилось начать игру, возможно игра уже запущена или недостаточно игроков'}, status=404)
+        return JsonResponse({'err':'Не получилось начать игру, возможно игра уже запущена или недостаточно игроков'}, status=400)
 
 
 @decorators.login_required(login_url='/accounts/login/')
@@ -72,13 +69,21 @@ def show_stat(request:HttpRequest, game_id:UUID):
     game_engine = GameEngine.objects.get(game_id=game_id)
     user = User.objects.get(id=request.user.id)
     game_user = GameUser.objects.filter(game_id=game_engine.game_id, account_id=user).first()
-    game_user.show_stat(statname)
-    sync_game(request, game_id)
-    return JsonResponse(list([]), safe=False)
+    try:
+        game_engine.show_stat(statname, game_user)
+    except exceptions.LobbyStatusCheckMismatch:
+        return JsonResponse({'err':'Игра ещё не началась'}, status=400)
+    except exceptions.WrongPlayerTurn:
+        return JsonResponse({'err':'Сейчас не ваш ход'}, status=400)
+    except exceptions.StatAlreadyShowed:
+        return JsonResponse({'err':'Вы уже показали эту карту'}, status=400)
+    sync_game(game_id)
+    return JsonResponse({'message':'Вы показали свою характеристику'}, status=200)
+
 
 @decorators.login_required(login_url='/accounts/login/')
 def leave_game(request:HttpRequest, game_id:UUID):
     game_engine = GameEngine.objects.get(game_id=game_id)
     game_engine.remove_user(request.user, )
-    sync_game(request, game_id)
+    sync_game(game_id)
     return redirect('/')
